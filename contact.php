b8026f921467dc30978128f5f9d90864008461f7
Jeevraj Rathore <98352214+Jeevraj-Rathore@users.noreply.github.com>
2022-04-22 20:57:03 +0530

    Contact

---
+++ contact.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Welcome to iDiscuss - Coding Forums</title>
+    <!-- <style>
+    .container{
+      min-height: 75vh;
+    }
+    </style> -->
+</head>
+
+<body>
+    <!-- <?php include 'partials/_dbconnect.php';?> -->
+    <?php include 'partials/_header.php';?>
+
+    <div class="container w-50 m-auto">
+        <div class="text">
+            <h1 class="text-center">Contact Us</h1>
+            <hr class="w-25 m-auto bg-dark">
+        </div>
+        <form action="action.php" method="POST" autocomplete="off">
+            <div class="user my-1">
+              <label for="username">Username: </label>
+              <input type="text" name="username" id="username" class="form-control" required>
+            </div>
+            <div class="email my-1">
+              <label for="email">E-mail: </label>
+              <input type="email" name="email" id="email" class="form-control" required>
+            </div>
+            <div class="contact my-1">
+              <label for="contact">Contact: </label>
+              <input type="number" name="contact" id="contact" class="form-control" required>
+            </div>
+            <div class="address my-1">
+              <label for="address">Address: </label>
+              <input type="text" name="address" id="address" class="form-control" required>
+            </div>
+            <div class="message my-1">
+              <label for="message">Message: </label>
+              <textarea name="message" id="message" class="form-control" cols="30" rows="5"></textarea>
+            </div>
+            <button class="btn btn-success my-1">Send Message</button>
+        </form>
+    </div>
+    <?php include 'partials/_footer.php';?>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
